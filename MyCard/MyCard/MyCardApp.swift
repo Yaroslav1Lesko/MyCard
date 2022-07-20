faae6d945b08747cfa4775b4a8f34b07e272f9cd
Yaroslav Lesko <99767670+Yaroslav1Lesko@users.noreply.github.com>
2022-07-20 18:43:54 +0300

    Card

---
+++ MyCard/MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by yaroslav on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
